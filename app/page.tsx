4d36c7a882b8311e789d8d01dfa51f089a7d54c6
jrajni <jrajni6@gmail.com>
2026-09-23 21:39:55 +0530

    readme updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import { MantineProvider } from "@mantine/core";
-import Image from "next/image";
-// core styles are required for all packages
-import "@mantine/core/styles.css";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <div></div>;
+  redirect("/start");
 }
